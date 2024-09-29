749c4d7244db2215c7059c40db41f2c2c0692067
Euclides <es7.comercial@gmail.com>
2024-09-29 18:37:25 +0200

    updated the 6 home cards and changed the document title

--- src/components/Home/BodyComponents/cardSection.jsx
+++ src/components/Home/BodyComponents/cardSection.jsx
@@ -1,7 +1,10 @@
 import { Fragment } from "react";
-import { LiaLaptopCodeSolid } from "react-icons/lia";
-import { FaTools } from "react-icons/fa";
 import { IoStorefront } from "react-icons/io5";
+import { FaChartLine } from "react-icons/fa6";
+import { PiCertificateFill } from "react-icons/pi";
+import { IoTimeSharp } from "react-icons/io5";
+import { FaPersonSkiing } from "react-icons/fa6";
+import { GiHiking } from "react-icons/gi";
 
 export default function CardSection() {
     return(
@@ -13,21 +16,22 @@ export default function CardSection() {
             <div className="card">
                 <div className="card-icon-square">
                     <div className="card-icon">
-                        <LiaLaptopCodeSolid className="icon" />
+                        <PiCertificateFill className="icon" />
                     </div>
                 </div>
                 <div className="card-content">
-                    <h1 className="content-text">Building <br /> your website</h1>
+                    <h1 className="content-text">Getting <br /> a certificate
+                    </h1>
                 </div>
             </div>
             <div className="card">
                 <div className="card-icon-square">
                     <div className="card-icon">
-                    <FaTools className="icon-2"/>
+                    <FaChartLine className="icon-2"/>
                     </div>
                 </div>
                 <div className="card-content">
-                    <h1>Using the <br /> best tools</h1>
+                    <h1 className="content-text">Developing <br /> your self </h1>
                 </div>
             </div>
             <div className="card">
@@ -37,7 +41,41 @@ export default function CardSection() {
                     </div>
                 </div>
                 <div className="card-content">
-                    <h1><span className="grow_text">Grow up</span> <br />your business</h1>
+                    <h1 className="content-text">Growing up <br /> your business </h1>
+                </div>
+            </div>
+        </div>
+        <div className="card-title">
+            </div>
+            <div className="cards">
+            <div className="card">
+                <div className="card-icon-square">
+                    <div className="card-icon">
+                        <FaPersonSkiing className="icon" />
+                    </div>
+                </div>
+                <div className="card-content">
+                    <h1 className="content-text">Improving <br /> your skills</h1>
+                </div>
+            </div>
+            <div className="card">
+                <div className="card-icon-square">
+                    <div className="card-icon">
+                    <IoTimeSharp className="icon-2"/>
+                    </div>
+                </div>
+                <div className="card-content">
+                    <h1 className="content-text">Mastering <br /> time managment </h1>
+                </div>
+            </div>
+            <div className="card">
+                <div className="card-icon-square">
+                    <div className="card-icon">
+                        <GiHiking className="icon"/>
+                    </div>
+                </div>
+                <div className="card-content">
+                    <h1 className="content-text">Building <br /> Lidership </h1>
                 </div>
             </div>
         </div> 
--- src/components/Home/BodyComponents/footer.jsx
+++ src/components/Home/BodyComponents/footer.jsx
@@ -1,7 +1,7 @@
 export default function BodyFooter() {
     return(
         <div className="footer-title-section">
-            <h1 className="footer-title">Just a simple text</h1>
+            <h1 className="footer-title">Enjoy our website</h1>
         </div>
     );
 }
